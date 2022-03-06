6f7a9088bfa34f3f0e5a394e49718f6c5df27f55
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2022-03-06 16:19:31 +0300

    randomiser

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,22 @@
 // TODO: write code here
-console.log('app.js included');
+console.log('app worked');
+const field = document.querySelector('.field');
+for (let i = 0; i < 16; i++) {
+  const block = document.createElement('div');
+  block.id = i;
+  block.classList.add('block');
+  field.appendChild(block);
+}
+const blocks = Array.from(document.querySelectorAll('.block'));
+console.log(blocks);
+function activateBlock() {
+  blocks.forEach((element) => {
+    if (element.classList.contains('activeBlock')) {
+      element.classList.remove('activeBlock');
+    }
+  });
+  const index = Math.floor(Math.random() * 15);
+  blocks[index].classList.add('activeBlock');
+}
+
+setInterval(activateBlock, 1000);
